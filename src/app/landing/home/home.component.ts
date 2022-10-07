926998229697874199e7b502e10f5d30e98ef6e0
Kuzinkov Dmitry <dmytro.kuzinkov@chanty.com>
2022-10-07 13:20:59 +0300

    minor fixes

--- src/app/landing/home/home.component.ts
+++ src/app/landing/home/home.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { EMenuIds } from 'src/app/enums/menu-ids.enum';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'wb-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent {
+
+  constructor(
+    private commonService: CommonService,
+  ) {}
+
+  public scrollToServicesBlock(): void {
+    this.commonService.scrollToElement(EMenuIds.Services);
+  }
+
+}
